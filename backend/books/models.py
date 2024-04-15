3486ec4bd1dc8f56b6277ad34833d1916ce24145
prx-2006 <seyfiabolfazl64@gmail.com>
2024-04-15 19:10:42 +0330

    backend(books/models/Book): init backend(users/models/Student): init

---
+++ backend/books/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Book(models.Model):
+    isbn = models.CharField(max_length = 13)
+    title = models.CharField(max_length = 120)
+    publisher = models.CharField(max_length = 100)
+    author = models.CharField(max_length = 150)
+    copies = models.IntegerField()
+    borrowable = models.BooleanField()
+
+    def __str__(self):
+        return self.title

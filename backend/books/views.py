a4c2905863b3fd6c926670aeb9dc598a239a7d1b
prx-2006 <seyfiabolfazl64@gmail.com>
2024-04-21 20:04:24 +0330

    backend(books/views/AddBook): init backend(books/serializers/BookSerializer): init

--- backend/books/views.py
+++ backend/books/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
+from .serializers import BookSerializer
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+
+
+@api_view(['POST'])
+def AddBook(request : Request):
+    serializer = BookSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+    else:
+        return Response(None, status.HTTP_400_BAD_REQUEST)

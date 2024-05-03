fa630a276dbc0fd47750fc09e8562e7529a64b7b
TJerf <85.erfantj@gmail.com>
2024-05-03 15:28:37 +0330

    frontend(bookList):add table for books ):

--- frontend/src/components/SearchField/index.jsx
+++ frontend/src/components/SearchField/index.jsx
@@ -7,11 +7,12 @@ import SearchIcon from "@mui/icons-material/Search";
 
 export default function SearchField() {
   return (
-    <form style={{ display: "flex" }}>
+    <form action="/Pokh" style={{ display: "flex" }}>
       <Button variant="contained" type="submit" color="primary" sx={{ mr: 1 }}>
         <SearchIcon />
       </Button>
       <TextField
+        name="q"
         id="search_bar"
         className="text"
         label="جست‌وجوی کتاب"

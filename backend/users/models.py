b671277734c45c86fb9a5f5802d246ae34c9845c
prx-2006 <seyfiabolfazl64@gmail.com>
2024-05-12 21:31:17 +0330

    backend(users/models/BorrowRecord): init

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,12 +2,19 @@ from books.models import Book
 from django.db import models
 
 
+
 class Student(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     class_number = models.IntegerField()
     national_code = models.IntegerField()
-    borrowed_book = models.ForeignKey(Book, on_delete=models.CASCADE)
-
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
+
+
+class BorrowRecord(models.Model):
+    is_returned = models.BooleanField(default=False)
+    book = models.OneToOneField(Book, on_delete=models.CASCADE, primary_key=True)
+    student = models.OneToOneField(Student, on_delete=models.CASCADE, default=None)
+    return_date = models.DateField()
